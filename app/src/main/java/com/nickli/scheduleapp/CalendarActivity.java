f0790906543df3c515b9f9aacfddda96154c0272
Nick Li <47701823+13ananaz@users.noreply.github.com>
2022-03-05 00:00:20 -0800

    Update CalendarActivity.java

--- app/src/main/java/com/nickli/scheduleapp/CalendarActivity.java
+++ app/src/main/java/com/nickli/scheduleapp/CalendarActivity.java
@@ -9,31 +9,41 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CalendarActivity extends AppCompatActivity {
+    // Define variables
     private WebView webView;
     Button btnBack;
 
     @Override
+    // When activity is created
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        // Sets frontend layout
         setContentView(R.layout.activity_calendar);
         webView = findViewById(R.id.wvCalendar);
         webView.setWebViewClient(new WebViewClient());
+        // Uses webView to load an updated calendar provided by school
         webView.loadUrl("https://www.mkhs.org/apps/events/calendar/?id=0");
+        // Implements zoom in and out controls for the activity
         webView.getSettings().setBuiltInZoomControls(true);
 
         btnBack = findViewById(R.id.btnBack);
 
+        // OnClickListener to change activities
         btnBack.setOnClickListener(view -> {
             startActivity(new Intent(CalendarActivity.this, MainActivity.class));
         });
     }
 
     @Override
+    // When Android back button pressed
     public void onBackPressed() {
+        // Makes sure if webView can go back
         if(webView.canGoBack()){
+            // Goes back in the website
             webView.goBack();
         }
         else {
+            // If webView cannot go back, go to the previous activity in the app
             super.onBackPressed();
         }
     }

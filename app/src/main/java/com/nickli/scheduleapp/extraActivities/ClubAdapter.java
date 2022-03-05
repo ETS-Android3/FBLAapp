3158e39cc5e5f64c3b8db83d1b898e16964394a8
Nick Li <47701823+13ananaz@users.noreply.github.com>
2022-03-04 23:16:23 -0800

    Update ClubAdapter.java

--- app/src/main/java/com/nickli/scheduleapp/extraActivities/ClubAdapter.java
+++ app/src/main/java/com/nickli/scheduleapp/extraActivities/ClubAdapter.java
@@ -15,12 +15,13 @@ import java.util.ArrayList;
 
 public class ClubAdapter extends RecyclerView.Adapter<ClubAdapter.MyViewHolder> {
 
-    //private final View.OnClickListener mOnClickListener = new MyOnClickListener();
+    // Define variables
     private ArrayList<ClubUpload> mList;
     private Context context;
 
     private RecyclerViewClickListener listener;
 
+    // Obtains list of information from database using ClubAdapter
     public ClubAdapter(Context context, ArrayList<ClubUpload> mList, RecyclerViewClickListener listener){
         this.context = context;
         this.mList = mList;
@@ -29,46 +30,46 @@ public class ClubAdapter extends RecyclerView.Adapter<ClubAdapter.MyViewHolder> 
 
     @NonNull
     @Override
+    // Takes list obtained from database and displays it onto a view holder (RecyclerView)
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(context).inflate(R.layout.email_item, parent ,false);
         return new MyViewHolder(v);
     }
 
     @Override
+    // Method to setText for the view holder and gets the information from the list
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.clubName.setText("Club: " + mList.get(position).getClubName());
         holder.clubStaff.setText("Staff(s): " + mList.get(position).getClubStaff());
     }
 
     @Override
+    // Gets size of list to see how many views are needed
     public int getItemCount() {
         return mList.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    // Class to implement RecyclerView
+    public class MyViewHolder extends RecyclerView.ViewHolder{
 
         TextView clubName;
         TextView clubStaff;
+
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             clubName = itemView.findViewById(R.id.staffName);
             clubStaff = itemView.findViewById(R.id.staffEmail);
-
-            itemView.setOnClickListener(this);
         }
 
-        @Override
-        public void onClick(View view) {
-            listener.onClick(view, getAdapterPosition());
-        }
     }
 
     public interface RecyclerViewClickListener{
         void onClick(View v, int position);
     }
 
+    // Method to change list to only filtered ones
     public void filterList(ArrayList<ClubUpload> filteredList) {
         mList = filteredList;
         notifyDataSetChanged();
